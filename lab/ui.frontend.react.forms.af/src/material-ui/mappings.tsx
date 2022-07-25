732e78caf1eb4830e60ef011c259e27a281bf776
Rishi Mehta <rismehta@adobe.com>
2022-07-25 14:44:15 +0530

    @trivial Adding code comments

--- lab/ui.frontend.react.forms.af/src/material-ui/mappings.tsx
+++
@@ -1,8 +0,0 @@
-import { mappings } from '@aemforms/af-react-components';
-import DateField from './Date'
-const newMappings: any = {
-    ...mappings,
-    'custom:dateField' : DateField
-};
-  
-export default newMappings;
